26edd11af99311ce9438626895d8736015a3da87
Max Maass <mmaass@seemoo.tu-darmstadt.de>
2019-03-22 16:40:42 +0100

    [Redocache] Parallelize cache regeneration

--- privacymail/util/management/commands/redocache.py
+++ privacymail/util/management/commands/redocache.py
@@ -3,6 +3,8 @@ from django.core.cache import cache
 from identity.models import Service
 from mailfetcher.models import Thirdparty
 from mailfetcher.analyser_cron import create_service_cache, create_summary_cache, create_third_party_cache
+from multiprocessing import cpu_count, Pool
+from functools import partial
 
 
 class Command(BaseCommand):
@@ -12,10 +14,20 @@ class Command(BaseCommand):
 
         create_summary_cache(force=True)
 
-        for service in Service.objects.all():
-            create_service_cache(service, force=True)
+        # Create multiprocessing pool, using 3/4th of available CPUs
+        # (leave a few CPUs available for handling other tasks)
+        pool = Pool(processes=int(cpu_count() / 4 * 3))
 
-        for thirdparty in Thirdparty.objects.all():
-            create_third_party_cache(thirdparty, force=True)
+        # Prepare service cache partial function
+        csc = partial(create_service_cache, force=True)
+        # Call in parallel
+        pool.map(csc, Service.objects.all())
 
+        # Prepare third party service cache partial function
+        ctpc = partial(create_third_party_cache, force=True)
+        # Call in parallel
+        pool.map(ctpc, Thirdparty.objects.all())
+
+        # Wait for tasks to terminate
+        pool.join()
         print('Done')
