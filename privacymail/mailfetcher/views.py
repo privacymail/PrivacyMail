8c979a6262e4daaf82a8795293a4ec8212abe254
Max Maass <mmaass@seemoo.tu-darmstadt.de>
2019-04-12 16:37:00 +0200

    [MailView] Re-check eMail spam status after new sender got approved

--- privacymail/mailfetcher/views.py
+++ privacymail/mailfetcher/views.py
@@ -22,5 +22,7 @@ def mailview(request, mail):
                 if request.POST['payload'] not in ident.service.permitted_senders:
                     ident.service.permitted_senders.append(request.POST['payload'])
                     ident.service.save()
+            # Check if the Mail is still fishy
+            mail.check_for_unusual_sender()
 
     return render(request, 'mailfetcher/mail.html', {'mail': mail})
