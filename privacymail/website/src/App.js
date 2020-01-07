a2d095abe110efdfe79bf5294115ace783be3613
Jonathan Schad <jonathan@jschad.de>
2020-01-07 16:52:01 +0100

    added a prallax effect

--- privacymail/website/src/App.js
+++ privacymail/website/src/App.js
@@ -14,17 +14,36 @@ function getRoutes() {
 
     return routes;
 }
-function App() {
-    return (
-        <div>
-            <Header />
-            <div className="content">
-                <BrowserRouter>
-                    <Switch>{getRoutes()}</Switch>
-                </BrowserRouter>
+class App extends React.Component {
+    componentDidMount() {
+        /**
+         * This is here in order to address changing viewports on android phones when a keyboard is opened.
+         * On the flipside on said phones the url bar now doesnt dissapers.
+         */
+        let viewheight = window.innerHeight;
+        let viewwidth = window.innerWidth;
+        let viewport = document.querySelector("meta[name=viewport]");
+        viewport.setAttribute(
+            "content",
+            "height=" +
+                viewheight +
+                "px, width=" +
+                viewwidth +
+                "px, initial-scale=1.0"
+        );
+    }
+    render() {
+        return (
+            <div>
+                <Header />
+                <div className="content">
+                    <BrowserRouter>
+                        <Switch>{getRoutes()}</Switch>
+                    </BrowserRouter>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default App;
--- privacymail/website/src/components/home/Home.js
+++ privacymail/website/src/components/home/Home.js
@@ -1,50 +1,69 @@
 import React from "react";
 import { Trans, useTranslation } from "react-i18next";
+import {
+    Parallax,
+    ParallaxBase,
+    ParallaxBackground
+} from "../../utils/Parallax";
+import letterImg from "../../assets/images/letter.jpg";
 
 function Home() {
     const { t } = useTranslation();
     return (
-        <div className="home grid">
-            <div></div>
-            <h1 className="grid-item light">
-                <Trans>home_headline</Trans>
-            </h1>
-            <h3 className="grid-item light">
-                <Trans>home_subheadline</Trans>
-            </h3>
-            <div className="grid-divider" />
-            <div className="grid-item input">
-                <input type="text" placeholder={t("home_inputPlaceholder")} />
-            </div>
-            <div className="grid-item">
-                <button>
-                    <Trans>home_analyise</Trans>
-                </button>
-            </div>
-            <div className="grid-divider" />
-            <div className="grid-item-4">
-                <p className="medium">69.933</p>
-                <p className="regular">
-                    <Trans>home_emailsAnalysied</Trans>
-                </p>
-            </div>
-            <div className="grid-item-4">
-                <p className="medium">2.928</p>
-                <p className="regular">
-                    <Trans>home_found3rdPraties</Trans>
-                </p>
-            </div>
-            <div className="grid-item-4">
-                <p className="medium highlighted">1.363</p>
-                <p className="regular">
-                    <Trans>home_registeredNewsletters</Trans>
-                </p>
-            </div>
+        <div className="home">
+            <Parallax>
+                <ParallaxBase>
+                    <div className="grid">
+                        <h1 className="grid-item light">
+                            <Trans>home_headline</Trans>
+                        </h1>
+                        <h3 className="grid-item light">
+                            <Trans>home_subheadline</Trans>
+                        </h3>
+                        <div className="grid-divider" />
+                        <div className="grid-item input">
+                            <input
+                                type="text"
+                                placeholder={t("home_inputPlaceholder")}
+                            />
+                            <button>
+                                <Trans>home_analyise</Trans>
+                            </button>
+                        </div>
+                        <div className="grid-divider" />
+                        <div className="grid-item-4">
+                            <p className="medium">69.933</p>
+                            <p className="regular">
+                                <Trans>home_emailsAnalysied</Trans>
+                            </p>
+                        </div>
+                        <div className="grid-item-4">
+                            <p className="medium">2.928</p>
+                            <p className="regular">
+                                <Trans>home_found3rdPraties</Trans>
+                            </p>
+                        </div>
+                        <div className="grid-item-4">
+                            <p className="medium highlighted">1.363</p>
+                            <p className="regular">
+                                <Trans>home_registeredNewsletters</Trans>
+                            </p>
+                        </div>
 
-            <div className="grid-divider" />
-            <h2 className="grid-item regular">
-                <Trans>home_shiningALight</Trans>
-            </h2>
+                        <div className="grid-divider" />
+                        <h2 className="grid-item regular">
+                            <Trans>home_shiningALight</Trans>
+                        </h2>
+                    </div>
+                </ParallaxBase>
+                <ParallaxBackground>
+                    <div className="backgroundImage">
+                        <div>
+                            <img src={letterImg} alt="" />
+                        </div>
+                    </div>
+                </ParallaxBackground>
+            </Parallax>
         </div>
     );
 }
