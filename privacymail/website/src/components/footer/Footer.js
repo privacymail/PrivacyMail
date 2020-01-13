429791ed9812da2bfc87345bea895d762ed375fb
Jonathan Schad <jonathan@jschad.de>
2020-01-13 17:25:11 +0100

    saving prgress with footer

--- privacymail/website/src/components/footer/Footer.js
+++ privacymail/website/src/components/footer/Footer.js
@@ -1,5 +1,26 @@
 import React from "react";
+import { Trans } from "react-i18next";
 function Footer() {
-    return <div className="footer"></div>;
+    return (
+        <div className="footer">
+            <div className="grid">
+                <div className="grid-item-4">
+                    <Trans>footer_language</Trans>
+                </div>
+                <div className="grid-item-4">
+                    <Trans>footer_faq</Trans>
+                </div>
+                <div className="grid-item-4">
+                    <Trans>footer_github</Trans>
+                </div>
+                <div className="grid-item-4">
+                    <Trans>footer_imprint</Trans>
+                </div>
+                <div className="grid-item-4">
+                    <Trans>footer_dataProtection</Trans>
+                </div>
+            </div>
+        </div>
+    );
 }
 export default Footer;
