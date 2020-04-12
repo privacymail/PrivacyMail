8e4f2af27abd0ad51c9981c9d79769318bd311e2
Jonathan Schad <jonathan@jschad.de>
2020-04-12 12:00:24 +0200

    added some translation keys

---
+++ privacymail/website/src/components/imprint/Imprint.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Trans } from "react-i18next";
+
+const Imprint = () => {
+    return <div>
+        <h1><Trans>imprint</Trans></h1>
+
+        <h2><Trans>imprint_headline1</Trans></h2>
+
+        <Trans>imprint_data1</Trans>
+    </div>
+}
+
+export default Imprint 
