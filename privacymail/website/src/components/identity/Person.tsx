ef5dcb5bb87e03e79f9ee72110bd183dd7fb2e04
Jonathan Schad <jonathan@jschad.de>
2020-04-25 19:15:40 +0200

    did some starge stuff with icons

--- privacymail/website/src/components/identity/Person.tsx
+++ privacymail/website/src/components/identity/Person.tsx
@@ -1,28 +1,52 @@
 import React from "react";
 import { IIdentity } from "../../repository/identity";
 
-import male from "../../assets/images/male.jpg";
-import female from "../../assets/images/female.jpg";
 import { Icon } from "../../utils/Icon";
+import { Trans } from "react-i18next";
 
 interface PersonProps {
     identity?: IIdentity;
 }
 const Person = (props: PersonProps) => {
     const copyToClipboard = () => {
+        console.log("click");
+
         navigator.clipboard.writeText(props.identity?.mail || "");
     };
 
     return (
         <div className="person">
-            <img
-                className="picture"
-                src={props.identity?.gender ? male : female}
-                alt={props.identity?.gender ? "male" : "female"}
-            />
-            <div className="email">
-                <p>{props.identity?.mail}</p>
-                <Icon onClick={copyToClipboard}>file_copy</Icon>
+            <div className={"profilePic " + (props.identity?.gender ? "male" : "female")}>
+                <div className="email">
+                    <p>{props.identity?.mail}</p>
+                    <Icon onClick={copyToClipboard}>file_copy</Icon>
+                </div>
+            </div>
+
+            <div className="otherInfo">
+                <p>
+                    <span className="medium">
+                        <Trans>identity_name</Trans>
+                        {": "}
+                    </span>
+                    {props.identity?.first_name} {props.identity?.surname}
+                </p>
+                <p>
+                    <span className="medium">
+                        <Trans>identity_gender</Trans>
+                        {": "}
+                    </span>
+                    {props.identity?.gender ? <Trans>male</Trans> : <Trans>female</Trans>}
+                </p>
+                <p>
+                    <span className="medium">
+                        <Trans>identity_website</Trans>
+                        {": "}
+                    </span>
+                    <a href={"http://" + props.identity?.service.url} target="_blank" rel="noopener noreferrer">
+                        {props.identity?.service.url}
+                    </a>
+                </p>
             </div>
         </div>
     );
