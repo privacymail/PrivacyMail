c2480abde5d0d147ef033a9a085ca69e1cfd387f
Jonathan Schad <jonathan@jschad.de>
2020-05-05 21:11:53 +0200

    added color to privacyrating

--- privacymail/website/src/components/newsletter/PrivacyRating.tsx
+++ privacymail/website/src/components/newsletter/PrivacyRating.tsx
@@ -36,9 +36,10 @@ const PrivacyRating = (props: PrivacyRatingProps) => {
     };
 
     const getRatingColor = (fadeFraction: number) => {
-        var color1 = { red: 76, green: 175, blue: 80 };
-        var color2 = { red: 251, green: 192, blue: 45 };
-        var fade = fadeFraction;
+        let color1 = { red: 76, green: 175, blue: 80 };
+        let color2 = { red: 251, green: 192, blue: 45 };
+        let color3 = { red: 227, green: 65, blue: 52 };
+        let fade = fadeFraction;
 
         // Do we have 3 colors for the gradient? Need to adjust the params.
 
@@ -48,14 +49,14 @@ const PrivacyRating = (props: PrivacyRatingProps) => {
         if (fade >= 1) {
             fade -= 1;
             color1 = color2;
-            color2 = { red: 227, green: 65, blue: 52 };
+            color2 = color3;
         }
 
-        var diffRed = color2.red - color1.red;
-        var diffGreen = color2.green - color1.green;
-        var diffBlue = color2.blue - color1.blue;
+        let diffRed = color2.red - color1.red;
+        let diffGreen = color2.green - color1.green;
+        let diffBlue = color2.blue - color1.blue;
 
-        var gradient = {
+        let gradient = {
             red: Math.floor(color1.red + diffRed * fade),
             green: Math.floor(color1.green + diffGreen * fade),
             blue: Math.floor(color1.blue + diffBlue * fade)
