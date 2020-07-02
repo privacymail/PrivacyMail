557848fd7d629c63bb620bd5a5a7dbba8d378a45
Jonathan Schad <jonathan@jschad.de>
2020-07-02 14:15:27 +0000

    newsletter warning now directs to identity add

--- privacymail/website/src/components/newsletter/IdentityAlert.tsx
+++ privacymail/website/src/components/newsletter/IdentityAlert.tsx
@@ -1,16 +1,22 @@
 import React from "react";
 import { Trans } from "react-i18next";
+import { Link } from "react-router-dom";
 
-const IdentityAlert = () => {
+interface IdentityAlertProps {
+    newsletterName: string;
+}
+
+const IdentityAlert = (props: IdentityAlertProps) => {
     return (
         <div className="alert warning identityAlert">
             <p>
                 <Trans>identity_lowIdentityCount</Trans>
             </p>
-
-            <button>
-                <Trans>identity_addIdentity</Trans>
-            </button>
+            <Link to={"/identity/" + props.newsletterName}>
+                <button id="analizeButton">
+                    <Trans>identity_addIdentity</Trans>
+                </button>
+            </Link>
         </div>
     );
 };
--- privacymail/website/src/components/newsletter/Newsletter.tsx
+++ privacymail/website/src/components/newsletter/Newsletter.tsx
@@ -27,7 +27,9 @@ const Newsletter = (props: NewsletterProps) => {
             <div className="newsletter">
                 <FaqHint />
                 <PrivacyRating privacyRating={newsletter?.rating} newsletter={newsletter?.service.name || ""} />
-                {newsletter && newsletter?.num_different_idents < 3 && <IdentityAlert />}
+                {newsletter && newsletter?.num_different_idents < 3 && (
+                    <IdentityAlert newsletterName={newsletter?.service.name} />
+                )}
                 <div className="divider" />
                 <GerneralInfo
                     entity={newsletter?.service}
