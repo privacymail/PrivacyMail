6c32e0d7fd6992a0b433ac5ecc2633908239c3b6
Jonathan Schad <jonathan@jschad.de>
2020-07-06 12:02:36 +0000

    added numberseperation to statisitcs

--- privacymail/website/src/components/home/Statistics.tsx
+++ privacymail/website/src/components/home/Statistics.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Trans } from "react-i18next";
 import { IStatistics, getStatistics } from "../../repository";
 
+import i18n from "../../i18n/i18n";
 const Statistics = () => {
     const [statistics, setStatistics] = useState<IStatistics>();
     useEffect(() => getStatistics(setStatistics), []);
@@ -9,19 +10,19 @@ const Statistics = () => {
     return (
         <div className="grid statistic">
             <div className="grid-item-4 ">
-                <h5>{statistics?.email_count ?? "..."}</h5>
+                <h5>{statistics?.email_count.toLocaleString(i18n.language) ?? "..."}</h5>
                 <p className="regular normal">
                     <Trans>home_emailsAnalysied</Trans>
                 </p>
             </div>
             <div className="grid-item-4 ">
-                <h5>{statistics?.tracker_count ?? "..."}</h5>
+                <h5>{statistics?.tracker_count.toLocaleString(i18n.language) ?? "..."}</h5>
                 <p className="regular normal">
                     <Trans>home_found3rdPraties</Trans>
                 </p>
             </div>
             <div className="grid-item-4 ">
-                <h5>{statistics?.service_count ?? "..."}</h5>
+                <h5>{statistics?.service_count.toLocaleString(i18n.language) ?? "..."}</h5>
                 <p className="regular normal">
                     <Trans>home_registeredNewsletters</Trans>
                 </p>
