81424408be21c49116f1f74424e4400923fab97a
Jan <gitlab@jidbo.de>
2021-01-27 13:30:22 +0000

    add docker setup

---
+++ _docker/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Making migrations and migrating the database. "
+source /opt/conda/etc/profile.d/conda.sh
+conda activate privacymail
+cd privacymail
+python manage.py makemigrations --noinput 
+python manage.py migrate --noinput 
+python manage.py collectstatic --noinput
+
+gunicorn privacymail.wsgi:application --bind 0.0.0.0:8000 --workers=4
