ba4a02dc96e5a4d21501501cb4c5e47aca08f649
Viet <dingscvv@googlemail.com>
2021-03-21 16:19:04 +0100

    indents corrected

--- privacymail/mailfetcher/views.py
+++ privacymail/mailfetcher/views.py
@@ -11,17 +11,17 @@ def mailview(request, mail):
     if request.method == 'POST' and request.POST['action']:
         if request.POST['action'] == 'markSpam':
             for ident in mail.identity.all():
-            ident.receives_third_party_spam = True
-            ident.save()
+                ident.receives_third_party_spam = True
+                ident.save()
         elif request.POST['action'] == 'markRegistered':
             for ident in mail.identity.all():
-            ident.approved = True
-            ident.save()
+                ident.approved = True
+                ident.save()
         elif request.POST['action'] == 'markPermittedSender':
             for ident in mail.identity.all():
-            if request.POST['payload'] not in ident.service.permitted_senders:
-                ident.service.permitted_senders.append(request.POST['payload'])
-                ident.service.save()
+                if request.POST['payload'] not in ident.service.permitted_senders:
+                    ident.service.permitted_senders.append(request.POST['payload'])
+                    ident.service.save()
             # Check if the Mail is still fishy
             mail.check_for_unusual_sender()
 
