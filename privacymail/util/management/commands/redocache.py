51d5b409b9a9add2051d15c89359dcc64394e094
Viet <dingscvv@googlemail.com>
2021-03-24 19:00:32 +0100

    upsi

--- privacymail/util/management/commands/redocache.py
+++ privacymail/util/management/commands/redocache.py
@@ -9,13 +9,13 @@ from mailfetcher.analyser_cron import (
     create_summary_cache,
     create_third_party_cache,
     multiprocessing_create_service_cache,
+    multiprocessing_create_thirdparty_cache
 )
 from django.db import connections
-
-
+import time
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-
+        t1 = time.time()
         cache.clear()
         self.stdout.write("Cleared cache\n")
 
@@ -24,8 +24,10 @@ class Command(BaseCommand):
             cpus = multiprocessing.cpu_count() - 3
         else:
             cpus = 1
-        services = Service.objects.all()
-        with multiprocessing.Pool(cpus) as p:
-            p.map(multiprocessing_create_service_cache, services)
 
+        with multiprocessing.Pool(cpus) as p:
+            p.map(multiprocessing_create_service_cache, Service.objects.all())
+            p.map(multiprocessing_create_thirdparty_cache, Thirdparty.objects.all())
+        t2 = time.time()
+        print(t2 - t1)
         print("Done")
